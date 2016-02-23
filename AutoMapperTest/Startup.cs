96f7f3a243bcce9cbd8dfa30b28d8a78cd0abc8b
Ming Wong <js923030238@hotmail.com>
2016-02-23 18:41:27 +0800

    同步處裡AutoMapper

---
+++ AutoMapperTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutoMapperTest.Startup))]
+namespace AutoMapperTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
